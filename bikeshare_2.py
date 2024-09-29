10dba26b76b502607856abb4b42a05d99eeb752f
rykertracy <rykertracy@gmail.com>
2024-09-29 14:44:18 -0400

    docs: Add clarity to user_stats function docstring

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -182,7 +182,7 @@ def trip_duration_stats(df):
 
 
 def user_stats(df):
-    """Displays statistics on bikeshare users."""
+    """Displays statistics on bikeshare users. \n Only applies to certain cities."""
 
     print('\nCalculating User Stats...\n')
     start_time = time.time()
