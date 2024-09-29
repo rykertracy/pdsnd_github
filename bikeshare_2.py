cbe408d461ae20ace3925cad06716d5ab364f9c8
rykertracy <rykertracy@gmail.com>
2024-09-29 14:41:23 -0400

    docs: Update load_city function docstring for specificity and grammar

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -75,7 +75,7 @@ def get_filters():
 
 def load_data(city, month, day):
     """
-    Loads data for the specified city and filters by month and day if 
+    Loads data for the specified city and filters by month and day, if 
     applicable.
     Args:
         (str) city - name of the city to analyze
